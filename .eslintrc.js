48d5609475eb8719277601626f1c66209da2e568
BillyCoding <gustavo.rocha.191066@hotmail.com>
2020-08-16 11:33:28 -0300

    Debug

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  extends: 'node',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+};
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+    const {name, email} = req.body;
+
+    const user = {
+        name,
+        email,
+    }
+    return res.json(user);
+})
+
+app.listen(3333, () => console.log('SERVER STARTED ON PORT 3333'));
